00c19a808c9736338b65ef4419508eca60b01a95
Ricardo <bergerrc@gmail.com>
2024-10-19 18:51:22 +0000

    Setting logos

--- sections/Logos.tsx
+++ sections/Logos.tsx
@@ -12,9 +12,9 @@ export interface Props {
   logos?: Logo[];
 }
 
-const IMG_PLACEHODLER = Array(30).fill(0).map(() => ({
+const IMG_PLACEHODLER = Array(5).fill(5).map(() => ({
   src:
-    "https://ozksgdmyrqcxcwhnbepg.supabase.co/storage/v1/object/public/assets/1527/03fbcc78-ca86-4616-a59a-b8aa18331a9c",
+    "https://deco-sites-assets.s3.sa-east-1.amazonaws.com/beeapps/00e5e919-90ff-486b-8d4d-a7a536ca37a2/png-transparent-aws-vector-brand-logos-icon-thumbnail.png",
   altText: "Logo",
 }));
 
@@ -30,7 +30,7 @@ export default function Logos({
             src={logo.src || ""}
             alt={logo.altText || ""}
             width={110}
-            height={25}
+            height={110}
           />
         );
       })}
@@ -40,8 +40,8 @@ export default function Logos({
     <div class="lg:container md:max-w-6xl lg:mx-auto mx-4 py-6 lg:py-14">
       <div class="flex flex-col gap-12">
         <p class="text-center text-lg">{title}</p>
-        <div class="relative w-full overflow-hidden h-11">
-          <div class="animate-sliding absolute top-0 left-0 flex flex-nowrap h-11">
+        <div class="relative w-full h-full overflow-hidden">
+          <div class="animate-sliding absolute top-0 left-0 flex flex-nowrap h-full">
             {slideContent}
           </div>
         </div>
